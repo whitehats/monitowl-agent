c3e351636078337c22dccfb52f29fc48b7224adb
Marek Hobler <marek@hobler.pl>
2015-04-08 14:16:06 +0200

    release 16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="monitowl-agent",
-    version="15",
+    version="16",
     description="MonitOwl.com Agent software",
     long_description=open("README.rst").read(),
     license="Apache Software License",
