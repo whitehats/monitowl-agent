4693c3242289a0228bcd116581a744d5ad399981
KenjiTakahashi <wozniakk@gmail.com>
2015-09-24 17:43:38 +0200

    release 21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="monitowl-agent",
-    version="20",
+    version="21",
     description="MonitOwl.com Agent software",
     long_description=open("README.rst").read(),
     license="Apache Software License",
